84ce3977927421fff981affcb939f1c925174d89
Mohamed Sahraoui <msahraoui@carvivo.com>
2018-04-24 22:06:19 +0200

    generate entities

---
+++ src/AppBundle/Entity/Commission.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Commission
+ *
+ * @ORM\Table(name="commission", indexes={@ORM\Index(name="idMerchant", columns={"idMerchant"}), @ORM\Index(name="idUser", columns={"idUser"})})
+ * @ORM\Entity
+ */
+class Commission
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=false)
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cashback", type="decimal", precision=10, scale=2, nullable=false)
+     */
+    private $cashback;
+
+    /**
+     * @var \AppBundle\Entity\Merchant
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Merchant")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="idMerchant", referencedColumnName="id")
+     * })
+     */
+    private $idmerchant;
+
+    /**
+     * @var \AppBundle\Entity\User
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="idUser", referencedColumnName="id")
+     * })
+     */
+    private $iduser;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Commission
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set cashback
+     *
+     * @param string $cashback
+     *
+     * @return Commission
+     */
+    public function setCashback($cashback)
+    {
+        $this->cashback = $cashback;
+
+        return $this;
+    }
+
+    /**
+     * Get cashback
+     *
+     * @return string
+     */
+    public function getCashback()
+    {
+        return $this->cashback;
+    }
+
+    /**
+     * Set idmerchant
+     *
+     * @param \AppBundle\Entity\Merchant $idmerchant
+     *
+     * @return Commission
+     */
+    public function setIdmerchant(\AppBundle\Entity\Merchant $idmerchant = null)
+    {
+        $this->idmerchant = $idmerchant;
+
+        return $this;
+    }
+
+    /**
+     * Get idmerchant
+     *
+     * @return \AppBundle\Entity\Merchant
+     */
+    public function getIdmerchant()
+    {
+        return $this->idmerchant;
+    }
+
+    /**
+     * Set iduser
+     *
+     * @param \AppBundle\Entity\User $iduser
+     *
+     * @return Commission
+     */
+    public function setIduser(\AppBundle\Entity\User $iduser = null)
+    {
+        $this->iduser = $iduser;
+
+        return $this;
+    }
+
+    /**
+     * Get iduser
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getIduser()
+    {
+        return $this->iduser;
+    }
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=50, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=64, nullable=true)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=50, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="profileUrl", type="string", length=255, nullable=true)
+     */
+    private $profileurl;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="lastLogin", type="datetime", nullable=true)
+     */
+    private $lastlogin = '0000-00-00 00:00:00';
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="creationDate", type="datetime", nullable=false)
+     */
+    private $creationdate = 'CURRENT_TIMESTAMP';
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set profileurl
+     *
+     * @param string $profileurl
+     *
+     * @return User
+     */
+    public function setProfileurl($profileurl)
+    {
+        $this->profileurl = $profileurl;
+
+        return $this;
+    }
+
+    /**
+     * Get profileurl
+     *
+     * @return string
+     */
+    public function getProfileurl()
+    {
+        return $this->profileurl;
+    }
+
+    /**
+     * Set lastlogin
+     *
+     * @param \DateTime $lastlogin
+     *
+     * @return User
+     */
+    public function setLastlogin($lastlogin)
+    {
+        $this->lastlogin = $lastlogin;
+
+        return $this;
+    }
+
+    /**
+     * Get lastlogin
+     *
+     * @return \DateTime
+     */
+    public function getLastlogin()
+    {
+        return $this->lastlogin;
+    }
+
+    /**
+     * Set creationdate
+     *
+     * @param \DateTime $creationdate
+     *
+     * @return User
+     */
+    public function setCreationdate($creationdate)
+    {
+        $this->creationdate = $creationdate;
+
+        return $this;
+    }
+
+    /**
+     * Get creationdate
+     *
+     * @return \DateTime
+     */
+    public function getCreationdate()
+    {
+        return $this->creationdate;
+    }
+}
